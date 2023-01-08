0046284451635e124a2932312ab081cfacee8737
Andrey <boombardir20@gmail.com>
2023-01-08 18:27:33 +0500

    end

--- Go-pinia/src/App.vue
+++ Go-pinia/src/App.vue
@@ -2,22 +2,37 @@
 import {useTodosStore} from "@/store/todos";
 import {ref} from "vue";
 
-const {todos, addTodo} = useTodosStore();
+import {storeToRefs} from "pinia"; // Why? типа реактивные данные
+
+const todosStore = useTodosStore();
+
+const {filteredTodos} = storeToRefs(todosStore); // Why? типа реактивные данные
+
+const {addTodo, toggleTodo, updateFilter} = todosStore; // любая функция котрая не связана с состоянием
 
 const newTodo = ref("");
+
+const addNewTodo = (text) => {
+  addTodo(text);
+  newTodo.value = "";
+}
 
 </script>
 
 <template>
   <main>
+    <div>
+      <button @click="updateFilter('all')">All</button>
+      <button @click="updateFilter('finished')">Finished</button>
+      <button @click="updateFilter('unfinished')">Unfinished</button>
+    </div>
     <input v-model="newTodo"/>
-    <button @click="addTodo(newTodo)">Add</button>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        <input type="checkbox" :checked="todo.done"/>
-        <span>{{ todo.text }}</span>
-      </li>
-    </ul>
+    <button @click="addNewTodo(newTodo)">Add</button>
+
+    <div v-for="todo in filteredTodos" :key="todo.id" style="display: flex">
+      <h1 :style="todo.done && {textDecoration: 'line-through'}">{{ todo.text }}</h1>
+      <button @click="toggleTodo(todo.id)">toggle</button>
+    </div>
   </main>
 </template>
 
--- Go-pinia/src/store/todos.js
+++ Go-pinia/src/store/todos.js
@@ -6,8 +6,25 @@ export const useTodosStore = defineStore("todos", {
             {id: 1, text: 'Learn Vue 3', done: true},
             {id: 2, text: 'Learn Pinia', done: false},
             {id: 3, text: 'Build something awesome', done: false},
+            {id: 4, text: 'Profit', done: false},
+            {id: 5, text: 'Profit 2', done: true},
+            {id: 6, text: 'Profit 3', done: false},
+            {id: 7, text: 'Profit 4', done: true},
+            {id: 8, text: 'Profit 5', done: false},
         ],
+        filter: 'all',
     }),
+    getters: {
+        filteredTodos() {
+            if (this.filter === 'finished') {
+                return this.todos.filter(todo => todo.done);
+            } else if (this.filter === 'unfinished') {
+                return this.todos.filter(todo => !todo.done);
+            } else {
+                return this.todos;
+            }
+        },
+    },
     actions: {
         addTodo(text) {
             if (!text) return;
@@ -16,6 +33,13 @@ export const useTodosStore = defineStore("todos", {
                 text: text,
                 done: false,
             })
-        }
+        },
+        toggleTodo(id) {
+            const todo = this.todos.find(todo => todo.id === id);
+            if (todo) todo.done = !todo.done;
+        },
+        updateFilter(value) {
+            this.filter = value;
+        },
     }
 })
